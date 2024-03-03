feb8bf54983214ef8813ccc95c8c8d821ab7a317
Kartik Sharma <69217796+kartik2828@users.noreply.github.com>
2024-03-03 21:20:06 +0530

    Update Day2.sql

--- Day2.sql
+++ Day2.sql
@@ -1,3 +1,11 @@
-Complete 50 Questions from Leetcode 
+Hello everyone! We're on Day 2, and these 50 questions are very great, I promise. You will gain confidence if you answer all of these questions correctly.
 
+I understand that some of you may be beginners, but don't worry—solutions are also available to assist you. However, the reason 
+I included this link on Day 2 is that I think it's crucial to practice alongside real-world challenges. 
+
+
+  ******************************************************************************************************************************************************************
+                                                                       Complete 50 Questions from Leetcode 
+
+  
 Top 50 questions -[ https://leetcode.com/studyplan/top-sql-50/]
